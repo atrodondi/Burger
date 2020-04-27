b67a63217d1b6f372b3c51ec44c44ab8daf631ef
Cheebachong <atrodondi@gmail.com>
2020-04-27 13:49:49 -0700

    got db displaying with handlebars on / path

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,16 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model burger.js to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
+
+router.get("/", function(req,res){
+    burger.selectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+module.exports = router;
