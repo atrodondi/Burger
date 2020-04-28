ea516a0238f044759a60bd3d17a3e310fb3c9585
Cheebachong <atrodondi@gmail.com>
2020-04-28 16:49:45 -0700

    cleaned up a bit of css and added some comments

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,8 @@ var router = express.Router();
 // Import the model burger.js to use its database functions.
 var burger = require("../models/burger.js");
 
+
+// gets all the burgers in the database upon load and render them to the index hbs file
 router.get("/", function(req,res){
     burger.selectAll(function(data){
         var hbsObject = {
@@ -14,12 +16,13 @@ router.get("/", function(req,res){
     });
 });
 
+// when a post is made to burgers api...we call the burger object (MODEL) and the create method, which is an extension of the ORM we used from the CAT activity
 router.post("/api/burgers", function(req, res) {
     burger.create(
       ["burger_name"],
      [req.body.burger_name],
      function(result) {
-      // Send back the ID of the new burger
+      // Send back the ID of new burger, and use that to render the corresponding item in the DB (newest burger)
       res.json({ id: result.insertId });
     });
   });
--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
+// creating a burger object that holds all of the methods we will need, eventually all of CRUD )we are missing destory right now
 var burger = {
     selectAll: function(cb){
         orm.selectAll("burgers", function(res){
